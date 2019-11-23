389a6322ab0f93dc4d24ec8ef0e800cfd4ea3403
waladi173040127 <waladi.173040127@mail.unpas.ac.id>
2019-11-24 02:02:43 +0700

    membuat footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,27 +1,51 @@
   <!-- Footer -->
-  <hr>
-  <footer class="">
-       <div class="card ">
-          <div class="card-header">
-            <ul class="nav " style="padding-top: 10px">
+
+  <footer>
+    <div class="container">
+      <hr>
+      <div class="row">
+        <div class="col-sm-8">
+             <ul class="nav " style="padding-top: 10px">
               <li class="nav-item ">
                 <a class="nav-link active" href="#" style="margin-left: 40px; font-weight: bold;">KangKaos</a>
               </li>
               <li class="nav-item">
                 <p class="nav-link" >situs online shop mudah & terpercaya</p>
               </li>
-              
             </ul>
-          </div>
-          <div class="card-body" style="margin-left: 50px;">
+        </div>
+        <div class="col-sm-4">
+          <ul class="nav " style="padding-top: 10px">
+              <li class="nav-item ">
+                <p class="nav-link active" href="#" style="margin-left: 40px; font-weight: bold;">Ulas kami di:</p>
+              </li>
+              <li class="nav-item">
+                <p class="nav-link" ></p>
+              </li>
+            </ul>
+        </div>
+      </div>
+      <hr>
+      <div class="row">
+        <div class="col-sm">
             <h5 class="card-title text-muted" style="font-weight: bold;">KangKaos</h5>
             <p class="card-text">Tentang Kami</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
         </div>
+        <div class="col-sm">
+            <h5 class="card-title text-muted" style="font-weight: bold;">Pembeli</h5>
+            <p class="card-text">Cara Belanja</p>
+        </div>
+        <div class="col-sm">
+            <h5 class="card-title text-muted" style="font-weight: bold;">KangKaos</h5>
+            <p class="card-text">Tentang Kami</p>
+        </div>
+      </div>
+    </div>
+    
     <!-- /.container -->
+    <hr>
       <div>
-       <p class=" text-center text-muted " style="line-height: -10px">Copyright &copy; Your Website 2019</p>
+       <p class=" text-center text-muted " style="line-height: -10px">Copyright &copy; KangKaos 2019</p>
       </div>
   </footer>
   <!-- Bootstrap core JavaScript -->
