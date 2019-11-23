a0e797b28aaf5f0343962057a4f0bd80f3eb0de6
waladi173040127 <waladi.173040127@mail.unpas.ac.id>
2019-11-23 20:18:20 +0700

    buat halaman home website

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,7 +3,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  class Home extends CI_Controller{
 
  	public function index(){
- 		echo "hai projek";
+ 		$data['judul'] = 'Home';
+ 		//$data['nama'] = $nama;
+ 		$this->load->view('templates/header');
+ 		$this->load->view('home/index');
+ 		$this->load->view('templates/footer');
  	}
  }
 
---
+++ application/views/home/index.php
@@ -0,0 +1,10 @@
+<div class="container">
+	<div class="jumbotron">
+	  <h1 class="display-4">Selamat Datang di Website Saya!</h1>
+	 
+	  <hr class="my-4">
+	  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+	  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+	</div>
+	<h1>waladi</h1>
+</div>
