2a2e7c38c9f2055ac0bd1e769292ff5195d8d1df
waladi173040127 <waladi.173040127@mail.unpas.ac.id>
2019-11-24 02:28:29 +0700

    card selesai

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -56,17 +56,16 @@
         <div class="row">
 
           <div class="col-lg-4 col-md-6 mb-4">
-            <div class="card h-100">
-              <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+            <div class="card" style="width: 23rem;">
+              <img src="http://placehold.it/700x400" class="card-img-top" alt="...">
               <div class="card-body">
-                <h4 class="card-title">
-                  <a href="#">Item One</a>
-                </h4>
-                <h5>$24.99</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-              </div>
-              <div class="card-footer">
-                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                <h5 class="card-title">Kaos Gaul cuk</h5>
+                <h6>RP.20.000</h6>
+                <p class="card-text">
+                  <small class="text-warning">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                </p>
+                <a href="#" class="btn btn-primary">Detail</a>
+                <a href="#" class="btn btn-danger">AddToCart</a>
               </div>
             </div>
           </div>
