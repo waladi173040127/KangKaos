4adc38be2d2da99923747b68f4de3bda280da808
Waladi Widarno <waladi.173040127@mail.unpas.ac.id>
2019-12-07 21:02:59 +0700

    menambahkan fungsi blocked.php

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -8,11 +8,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     }
 
  	public function index(){
-        
-         $data['title'] = 'Login Page';
+         if ($this->session->userdata('email')) {
+            redirect('user');
+        }
+
+        $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+        $this->form_validation->set_rules('password', 'Password', 'trim|required');
+
+        if ($this->form_validation->run() == false) {
+             $data['title'] = 'Login Page';
             $this->load->view('templates/auth_header', $data);
             $this->load->view('auth/login');
             $this->load->view('templates/auth_footer');
+        } else {
+            // validasinya success
+            $this->_login();
+        }
+        
  	}
 
  	public function registration(){
@@ -55,6 +67,56 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  		$this->load->view('auth/forget_password');
  		$this->load->view('templates/auth_footer');	
  	}
+    private function _login()
+    {
+        $email = $this->input->post('email');
+        $password = $this->input->post('password');
+
+        $user = $this->db->get_where('user', ['email' => $email])->row_array();
+
+        // jika usernya ada
+        if ($user) {
+            // jika usernya aktif
+            if ($user['is_active'] == 1) {
+                // cek password
+                if (password_verify($password, $user['password'])) {
+                    $data = [
+                        'email' => $user['email'],
+                        'role_id' => $user['role_id']
+                    ];
+                    $this->session->set_userdata($data);
+                    if ($user['role_id'] == 1) {
+                        redirect('admin');
+                    } else {
+                        redirect('user');
+                    }
+                } else {
+                    $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Wrong password!</div>');
+                    redirect('auth');
+                }
+            } else {
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">This email has not been activated!</div>');
+                redirect('auth');
+            }
+        } else {
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Email is not registered!</div>');
+            redirect('auth');
+        }
+    }
+
+    public function logout()
+    {
+        $this->session->unset_userdata('email');
+        $this->session->unset_userdata('role_id');
+
+        $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">You have been logged out!</div>');
+        redirect('auth');
+    }
+
+     public function blocked()
+    {
+        $this->load->view('auth/blocked');
+    }
  }
 
   ?>        
