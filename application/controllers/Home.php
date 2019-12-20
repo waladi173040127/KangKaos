711a1f26b1f912039a5b9dc22ba4819234e801a9
Waladi Widarno <waladi.173040127@mail.unpas.ac.id>
2019-12-20 20:57:58 +0700

    merapihkan halaman admin

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -11,9 +11,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  		$data['title'] = 'Home';
  		$data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
  		$data['produk'] = $this->db->get('produk')->result_array();
- 		 		$this->load->view('templates/home_header2', $data); 
+ 		$this->load->view('templates/home_header', $data); 
  		$this->load->view('home/index', $data);
- 		$this->load->view('templates/home_footer2');
+ 		$this->load->view('templates/home_footer');
  	}
  }
 
