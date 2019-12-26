6a51651b6d93b742ba612b195f1a082493021164
unknown <syachrialardhian50@gmail.com>
2019-12-26 15:44:05 -0800

    Menambahkan fungsi category di admin & CRUD produk

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -11,11 +11,36 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  		$data['title'] = 'Produk KangKaos';
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
         $data['produk'] = $this->Produk_model->tampil_data()->result();
-       $this->load->view('templates/header', $data);
+        $this->load->view('templates/header', $data);
         $this->load->view('templates/sidebar', $data);
         $this->load->view('templates/topbar', $data);
         $this->load->view('admin/index', $data);
         $this->load->view('templates/footer');
  	}
+
+    public function man(){
+        $data['title'] = 'Man';
+        $data['title2'] = 'Produk MAN';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        $data['produk'] = $this->db->get('produk')->result_array();
+        $data['man'] = $this->Category_model->data_man()->result();
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('admin/man', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function woman(){
+        $data['title'] = 'Woman';
+        $data['title2'] = 'Produk Woman';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        $data['woman'] = $this->Category_model->data_woman()->result();
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('admin/woman', $data);
+        $this->load->view('templates/footer');
+    }
 
  }
