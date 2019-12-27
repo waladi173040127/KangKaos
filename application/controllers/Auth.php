e35ec5fa661225e564e7efc962955cabfb91813f
unknown <syachrialardhian50@gmail.com>
2019-12-27 15:51:16 -0800

    Menambahkan fungsi aktifasi email

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -52,15 +52,97 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 'image' => 'default.jpg',
                 'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
                 'role_id' => 2,
-                'is_active' => 1,
+                'is_active' => 0,
                 'date_created' => time()
             ];
+
+            // Siapkan Token 
+            $token = base64_encode(random_bytes(32));
+            $user_token = [
+                'email' => $email,
+                'token' => $token,
+                'date_created' => time()
+            ];
+
             $this->db->insert('user', $data);
+            $this->db->insert('user_token', $user_token);
+
+            $this->_sendEmail($token, 'verify');
+
             $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Congratulation! your account has been created. Please activate your account</div>');
             redirect('auth');
         }
 			
  	}
+
+    private function _sendEmail($token, $type){
+        $config = [
+            'protocol' => 'smtp',
+            'smtp_host' => 'ssl://smtp.googlemail.com',
+            'smtp_user' => 'syachrialardhian50@gmail.com',
+            'smtp_pass' => 'ardhian1999',
+            'smtp_port' => 465,
+            'mailtype' => 'html',
+            'charset' => 'utf-8',
+            'newline' => "\r\n"
+        ];
+
+        $this->load->library('email', $config);
+        $this->email->initialize($config);
+
+        $this->email->from('syachrialardhian50@gmail.com', 'ardhian');
+        $this->email->to($this->input->post('email'));
+
+        if($type == 'verify'){
+             $this->email->subject('Account Verification');
+             $this->email->message('Click this link to verify you account : <a href="'. base_url(). 'auth/verify?email=' . $this->input->post('email') .'&token=' . urlencode($token) . '">Activate</a>');
+        }
+       
+
+        if($this->email->send()){
+            return true;
+        }else{
+            echo $this->email->print_debugger();
+            die;
+        }
+    }
+
+    public function verify(){
+        $email = $this->input->get('email');
+        $token = $this->input->get('token');
+
+        $user = $this->db->get_where('user', ['email' => $email])->row_array();
+
+        if($user){
+            $user_token = $this->db->get_where('user_token', ['token' => $token])->row_array();
+
+            if($user_token){
+                if(time() - $user_token['date_created'] < (60*60*24)){
+                    $this->db->set('is_active', 1);
+                    $this->db->where('email', $email);
+                    $this->db->update('user');
+
+                    $this->db->delete('user_token', ['email' => $email]);
+
+                      $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">'. $email .' has been activated! Please Login.</div>');
+                redirect('auth');
+                }else{
+
+                    $this->db->delete('user', ['email' => $email]);
+                     $this->db->delete('user_token', ['email' => $email]);
+                    $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Account activation Failed! Wrong token expired.</div>');
+                    redirect('auth');
+                }
+
+            }else{
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Account activation Failed! Wrong token.</div>');
+                redirect('auth');
+            }
+        }else{
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Account activation Failed! Wrong email.</div>');
+            redirect('auth');
+        }
+    }
 
  	public function forgetPassword(){
  		$data['title'] = 'Page registration';
